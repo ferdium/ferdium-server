93664eef67dfbbd8d9ac66b5d25c453746d0c954
vantezzen <properly@protonmail.com>
2019-08-25 13:55:26 +0200

    Add announcement route

--- start/routes.js
+++ start/routes.js
@@ -52,7 +52,7 @@ Route.group(() => {
   Route.get('services', 'StaticController.emptyArray')
   Route.get('news', 'StaticController.emptyArray')
   Route.get('payment/plans', 'StaticController.plans')
-  // Route.get('announcements/:version', 'StaticController.announcement')
+  Route.get('announcements/:version', 'StaticController.announcement')
 }).prefix('v1')
 
 // Dashboard
