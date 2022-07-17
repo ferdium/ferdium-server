ec344b0349b9c8a97b23ced65dddb1a865ef6ea2
Ricardo Cino <ricardo@cino.io>
2022-07-17 10:19:01 +0200

    chore: comply to new routing syntax by replace . with / in routes/resource locations

--- start/routes/web.ts
+++ start/routes/web.ts
@@ -6,34 +6,34 @@ if (Env.get('IS_DASHBOARD_ENABLED') !== 'false') {
   Route.group(() => {
     // Guest troutes
     Route.group(() => {
-      Route.get('login', 'Dashboard/LoginController.show');
-      Route.post('login', 'Dashboard/LoginController.login').as('login');
+      Route.get('login', 'Dashboard/LoginController/show');
+      Route.post('login', 'Dashboard/LoginController/login').as('login');
 
       // Reset password
-      Route.get('forgot', 'Dashboard/ForgotPasswordController.show');
-      Route.post('forgot', 'Dashboard/ForgotPasswordController.forgotPassword');
+      Route.get('forgot', 'Dashboard/ForgotPasswordController/show');
+      Route.post('forgot', 'Dashboard/ForgotPasswordController/forgotPassword');
 
-      Route.get('reset', 'Dashboard/ResetPasswordController.show');
+      Route.get('reset', 'Dashboard/ResetPasswordController/show');
 
       // change route
-      Route.post('reset', 'Dashboard/ResetPasswordController.resetPassword');
+      Route.post('reset', 'Dashboard/ResetPasswordController/resetPassword');
     }).middleware('guest');
 
     // Authenticated routes
     Route.group(() => {
-      Route.get('account', 'Dashboard/AccountController.show');
-      Route.post('account', 'Dashboard/AccountController.store');
+      Route.get('account', 'Dashboard/AccountController/show');
+      Route.post('account', 'Dashboard/AccountController/store');
 
-      Route.get('data', 'Dashboard/DataController.show');
-      Route.get('export', 'DashboardController.export');
+      Route.get('data', 'Dashboard/DataController/show');
+      Route.get('export', 'DashboardController/export');
 
-      Route.get('transfer', 'Dashboard/TransferController.show');
-      Route.post('transfer', 'Dashboard/TransferController.import');
+      Route.get('transfer', 'Dashboard/TransferController/show');
+      Route.post('transfer', 'Dashboard/TransferController/import');
 
-      Route.get('delete', 'Dashboard/DeleteController.show');
-      Route.post('delete', 'Dashboard/DeleteController.delete');
+      Route.get('delete', 'Dashboard/DeleteController/show');
+      Route.post('delete', 'Dashboard/DeleteController/delete');
 
-      Route.get('logout', 'Dashboard/LogOutController.logout');
+      Route.get('logout', 'Dashboard/LogOutController/logout');
 
       Route.get('*', ({ response }) => response.redirect('/user/account'));
     }).middleware('auth:web');
@@ -51,18 +51,18 @@ if (Env.get('IS_DASHBOARD_ENABLED') !== 'false') {
 }
 
 // Health check
-Route.get('health', 'HealthController.index');
+Route.get('health', 'HealthController/index');
 
 // Franz/Ferdi account import
-Route.get('import', ({ view }) => view.render('others.import'));
-Route.post('import', 'UserController.import');
+Route.get('import', ({ view }) => view.render('others/import'));
+Route.post('import', 'UserController/import');
 
 // Legal documents
 Route.get('terms', ({ response }) => response.redirect('/terms.html'));
 Route.get('privacy', ({ response }) => response.redirect('/privacy.html'));
 
 // Index
-Route.get('/', ({ view }) => view.render('others.index'));
+Route.get('/', ({ view }) => view.render('others/index'));
 
 // 404 handler
 Route.get('/*', ({ response }) => response.redirect('/'));
