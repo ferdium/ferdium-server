9fdd7294e2e7800de9fea1cafb70414cda04b03e
Ricardo Cino <ricardo@cino.io>
2022-07-19 13:47:00 +0200

    chore: add account-page and tests

--- app/Controllers/Http/Dashboard/AccountController.ts
+++ app/Controllers/Http/Dashboard/AccountController.ts
@@ -1,17 +1,75 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { schema, rules, validator } from '@ioc:Adonis/Core/Validator';
 
 export default class AccountController {
   /**
    * Shows the user account page
    */
-  public async show({}: HttpContextContract) {
-    // inplement your logic here
+  public async show({ auth, view }: HttpContextContract) {
+    return view.render('dashboard/account', {
+      username: auth.user?.username,
+      email: auth.user?.email,
+      lastname: auth.user?.lastname,
+    });
   }
 
   /**
    * Stores user account data
    */
-  public async store() {
-    // inplement your logic here
+  public async store({
+    auth,
+    request,
+    response,
+    session,
+    view,
+  }: HttpContextContract) {
+    try {
+      await validator.validate({
+        schema: schema.create({
+          username: schema.string([
+            rules.required(),
+            rules.unique({
+              table: 'users',
+              column: 'username',
+              caseInsensitive: true,
+              whereNot: { id: auth.user?.id },
+            }),
+          ]),
+          email: schema.string([
+            rules.required(),
+            rules.unique({
+              table: 'users',
+              column: 'email',
+              caseInsensitive: true,
+              whereNot: { id: auth.user?.id },
+            }),
+          ]),
+          lastname: schema.string([rules.required()]),
+        }),
+        data: request.only(['username', 'email', 'lastname']),
+      });
+    } catch (error) {
+      session.flash(error.messages);
+      return response.redirect('/user/account');
+    }
+
+    // Update user account
+    const { user } = auth;
+    if (user) {
+      user.username = request.input('username');
+      user.lastname = request.input('lastname');
+      user.email = request.input('email');
+      if (request.input('password')) {
+        user.password = request.input('password');
+      }
+      await user.save();
+    }
+
+    return view.render('dashboard/account', {
+      username: user?.username,
+      lastname: user?.lastname,
+      email: user?.email,
+      success: user !== undefined,
+    });
   }
 }
