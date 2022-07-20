37c0b59a4cd94dffc10fc46c7fcafe594c17e733
Ricardo Cino <ricardo@cino.io>
2022-07-20 21:01:12 +0200

    chore: add export and basic test

--- app/Controllers/Http/Dashboard/ExportController.ts
+++ app/Controllers/Http/Dashboard/ExportController.ts
@@ -1,10 +1,26 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import User from 'App/Models/User';
 
 export default class ExportController {
   /**
    * Display the export page
    */
-  public async show({ view }: HttpContextContract) {
-    // todo
+  public async show({ auth, response }: HttpContextContract) {
+    const user = auth.user as User;
+    const services = await user.related('services').query();
+    const workspaces = await user.related('workspaces').query();
+
+    const exportData = {
+      username: user.username,
+      lastname: user.lastname,
+      mail: user.email,
+      services: JSON.parse(JSON.stringify(services)),
+      workspaces: JSON.parse(JSON.stringify(workspaces)),
+    };
+
+    return response
+      .header('Content-Type', 'application/force-download')
+      .header('Content-disposition', 'attachment; filename=export.ferdium-data')
+      .send(exportData);
   }
 }
--- database/factories/UserFactory.ts
+++ database/factories/UserFactory.ts
@@ -1,9 +1,14 @@
 import User from 'App/Models/User';
 import Factory from '@ioc:Adonis/Lucid/Factory';
+import WorkspaceFactory from './WorkspaceFactory';
+import ServiceFactory from './ServiceFactory';
 
 export default Factory.define(User, async ({ faker }) => ({
   email: faker.internet.email(),
   username: faker.internet.userName(),
   password: 'password',
   lastname: faker.name.lastName().replace(/'/g, ''),
-})).build();
+}))
+  .relation('workspaces', () => WorkspaceFactory)
+  .relation('services', () => ServiceFactory)
+  .build();
