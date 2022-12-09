75950bb8889b7d730772b30270a30a071e2c790b
Vijay A <vraravam@users.noreply.github.com>
2022-12-09 06:22:20 +0530

    Removed API endpoints and other code related to pricing/payment plans - since Ferdium is completely FOSS

--- app/Controllers/Http/StaticController.js
+++ app/Controllers/Http/StaticController.js
@@ -12,61 +12,19 @@ class StaticController {
   }) {
     return response.send({
       needToWaitToProceed: false,
-      isSpellcheckerPremiumFeature: false,
-      isSpellcheckerIncludedInCurrentPlan: true,
       isServiceProxyEnabled: true,
-      isServiceProxyIncludedInCurrentPlan: true,
-      isServiceProxyPremiumFeature: true,
-      isWorkspacePremiumFeature: false,
       isWorkspaceEnabled: true,
       isAnnouncementsEnabled: true,
       isSettingsWSEnabled: false,
       isServiceLimitEnabled: false,
       serviceLimitCount: 0,
-      isCommunityRecipesPremiumFeature: false,
-      isCommunityRecipesIncludedInCurrentPlan: true,
-      isCustomUrlIncludedInCurrentPlan: true,
       isMagicBarEnabled: true,
-      isTeamManagementIncludedInCurrentPlan: true,
       isTodosEnabled: true,
-      isTodosIncludedInCurrentPlan: true,
-      defaultTrialPlan: 'franz-pro-yearly',
       subscribeURL: 'https://ferdium.org',
-      planSelectionURL: 'https://ferdium.org',
       hasInlineCheckout: true,
       isPlanSelectionEnabled: false,
       isTrialStatusBarEnabled: false,
       canSkipTrial: true,
-      pricingConfig: {
-        currency: '$',
-        currencyID: 'USD',
-        plans: {
-          personal: {
-            monthly: {
-              id: 'ferdium-free',
-              price: 0,
-              billed: 0,
-            },
-            yearly: {
-              id: 'ferdium-completely-free',
-              price: 0,
-              billed: 0,
-            },
-          },
-          pro: {
-            monthly: {
-              id: 'ferdium-still-free',
-              price: 0,
-              billed: 0,
-            },
-            yearly: {
-              id: 'ferdium-forever-free',
-              price: 0,
-              billed: 0,
-            },
-          },
-        },
-      },
     });
   }
 
@@ -75,22 +33,6 @@ class StaticController {
     response,
   }) {
     return response.send([]);
-  }
-
-  // Payment plans availible
-  plans({
-    response,
-  }) {
-    return response.send({
-      month: {
-        id: 'franz-supporter-license',
-        price: 99,
-      },
-      year: {
-        id: 'franz-supporter-license-year-2019',
-        price: 99,
-      },
-    });
   }
 
   // Show announcements
