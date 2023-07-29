e658ce34d9171dbb1a775575d4fe9cee30c81f29
MCMXC <16797721+mcmxcdev@users.noreply.github.com>
2023-07-29 14:17:37 -0600

    - add test command to prepare-code - disable strictPropertyInitialization flag in tsconfig which creates issues with adonis models - update precommit hook to excute pnpm prepare-code - remove ts-ignore statements from all models

--- app/Models/Recipe.ts
+++ app/Models/Recipe.ts
@@ -3,29 +3,21 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
 
 export default class Recipe extends BaseModel {
   @column({ isPrimary: true })
-  // @ts-ignore
   public id: number;
 
   @column()
-  // @ts-ignore
   public name: string;
 
   @column()
-  // @ts-ignore
   public recipeId: string;
 
   // TODO: Type the data object.
   @column()
-  // @ts-ignore
   public data: object;
 
-  // @ts-ignore
   @column.dateTime({ autoCreate: true })
-  // @ts-ignore
   public createdAt: DateTime;
 
-  // @ts-ignore
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  // @ts-ignore
   public updatedAt: DateTime;
 }
--- app/Models/Token.ts
+++ app/Models/Token.ts
@@ -4,40 +4,29 @@ import User from './User';
 
 export default class Token extends BaseModel {
   @column({ isPrimary: true })
-  // @ts-ignore
   public id: number;
 
-  // @ts-ignore
   @hasOne(() => User, {
     localKey: 'user_id',
     foreignKey: 'id',
   })
-  // @ts-ignore
   public user: HasOne<typeof User>;
 
   @column()
-  // @ts-ignore
   public user_id: number;
 
   @column()
-  // @ts-ignore
   public token: string;
 
   @column()
-  // @ts-ignore
   public type: string;
 
   @column()
-  // @ts-ignore
   public is_revoked: boolean;
 
-  // @ts-ignore
   @column.dateTime({ autoCreate: true })
-  // @ts-ignore
   public created_at: DateTime;
 
-  // @ts-ignore
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  // @ts-ignore
   public updated_at: DateTime;
 }
