195dd985ca8211476a43218f8a1a84c29071b5d8
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2023-09-27 17:37:28 +0100

    Fix issue with production database

--- database/migrations/1658076326250_correct_token_relations.ts
+++ database/migrations/1658076326250_correct_token_relations.ts
@@ -4,6 +4,10 @@ export default class extends BaseSchema {
   protected tableName = 'tokens';
 
   public async up(): Promise<void> {
+    await this.db.rawQuery(
+      'DELETE FROM tokens WHERE user_id NOT IN (SELECT id FROM users)',
+    );
+
     this.schema.alterTable(this.tableName, table => {
       table.dropForeign('user_id');
 
